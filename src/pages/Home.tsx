f34c4155d77331bf1e48b952f040d32acf0cffff
thyago608 <thyagoribeiro608@gmail.com>
2021-12-27 15:03:49 -0400

    Create struct page Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,34 @@
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
+
+export function Home() {
+  return (
+    <>
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Cria salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+
+      <main>
+        <div>
+          <img src={logoImg} alt="Logo do Google" />
+          <button type="button">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div>ou entre em uma sala</div>
+
+          <form action="">
+            <input type="text" placeholder="Digite o código da sala" />
+            <button type="submit">Entrar na sala</button>
+          </form>
+        </div>
+      </main>
+    </>
+  );
+}
