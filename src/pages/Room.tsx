78b75de4fb74b91ac2dfb383c29d0bb8d5430805
thyago608 <thyagoribeiro608@gmail.com>
2021-12-30 09:54:41 -0400

    Created the copy room code button

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,15 +1,23 @@
+import { useParams } from "react-router-dom";
 import logoImg from "assets/images/logo.svg";
 import { Button } from "components/Button";
+import { RoomCode } from "components/RoomCode";
 
 import "styles/room.scss";
 
+type RoomParams = {
+  id: string;
+};
+
 export function Room() {
+  const { id } = useParams<RoomParams>();
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <div>código</div>
+          <RoomCode code={id as string} />
         </div>
       </header>
 
