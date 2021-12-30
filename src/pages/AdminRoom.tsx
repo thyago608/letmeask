4ecfe25bde9f2a33b329f0eb45ab72aba7f3a563
thyago608 <thyagoribeiro608@gmail.com>
2021-12-30 19:58:58 -0400

    Created and stylized page AdminRoom

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,92 @@
+import { useRef, FormEvent, useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { useAuth } from "hooks/useAuth";
+import { useRoom } from "hooks/useRoom";
+import { Button } from "components/Button";
+import { RoomCode } from "components/RoomCode";
+import { Question } from "components/Question";
+import { database } from "services/firebase";
+import * as firebaseDatabase from "firebase/database";
+
+import logoImg from "assets/images/logo.svg";
+
+import "styles/room.scss";
+
+type RoomParams = {
+  id: string;
+};
+
+export function AdminRoom() {
+  const { id } = useParams<RoomParams>();
+  const { user } = useAuth();
+  const { title, questions } = useRoom(id as string);
+  const questionRef = useRef<HTMLTextAreaElement>(null);
+
+  async function handleSendQuestion(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    //Se o usuário não estiver autenticado
+    if (!user) {
+      throw new Error("You must be logged in");
+    }
+
+    //Se após retirar os espaços laterais o valor que está no textarea estiver vazio.
+    if (questionRef.current?.value.trim() === "") {
+      return;
+    }
+
+    const question = {
+      content: questionRef.current?.value,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isAnswered: false,
+      isHighlighted: false,
+    };
+
+    //Referênciando um local dentro do banco de dados
+    const questionsRef = firebaseDatabase.ref(
+      database,
+      `rooms/${id}/questions`
+    );
+
+    await firebaseDatabase.push(questionsRef, question);
+
+    questionRef.current!.value = "";
+  }
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <Link to="/">
+            <img src={logoImg} alt="Letmeask" />
+          </Link>
+
+          <div>
+            <RoomCode code={id as string} />
+            <Button isOutlined>Encerrar Sala</Button>
+          </div>
+        </div>
+      </header>
+
+      <main className="content">
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+        </div>
+
+        <div className="question-list">
+          {questions.map((question) => (
+            <Question
+              key={question.id}
+              author={question.author}
+              content={question.content}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "pages/Home";
 import { NewRoom } from "pages/NewRoom";
 import { Room } from "pages/Room";
+import { AdminRoom } from "pages/AdminRoom";
 
 export function RoutesApp() {
   return (
@@ -10,6 +11,7 @@ export function RoutesApp() {
         <Route path="/" element={<Home />} />
         <Route path="rooms/new" element={<NewRoom />} />
         <Route path="rooms/:id" element={<Room />} />
+        <Route path="admin/rooms/:id" element={<AdminRoom />} />
       </Routes>
     </BrowserRouter>
   );
