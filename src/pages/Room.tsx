982f46da1f8e2fa1041f6a6b0a9128dd3f7d4a38
thyago608 <thyagoribeiro608@gmail.com>
2021-12-30 09:22:53 -0400

    Page room stylized

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,3 +1,34 @@
+import logoImg from "assets/images/logo.svg";
+import { Button } from "components/Button";
+
+import "styles/room.scss";
+
 export function Room() {
-  return <div>Room</div>;
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <div>código</div>
+        </div>
+      </header>
+
+      <main className="content">
+        <div className="room-title">
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form>
+          <textarea placeholder=" O que você quer perguntar ?" />
+          <div className="form-footer">
+            <span>
+              Para enviar uma pergunta, <button>faça seu login</button>
+            </span>
+            <Button type="submit">Faça o seu login</Button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 }
